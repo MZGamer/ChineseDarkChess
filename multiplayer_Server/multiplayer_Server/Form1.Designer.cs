b8ed4ec0f9e87bec7872652531f87d43da219988
MZGamer <58198679+MZGamer@users.noreply.github.com>
2022-01-05 21:19:59 +0800

    V0.2
    加了輸贏判定
    能更直觀看到兩個玩家是否加入
    一些無關緊要的狀態顯示

---
+++ multiplayer_Server/multiplayer_Server/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace multiplayer_Server {
+    partial class serverGUI {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ConsoleMessage = new System.Windows.Forms.TextBox();
+            this.PLAYER1IP = new System.Windows.Forms.Label();
+            this.GameStatusLabel = new System.Windows.Forms.Label();
+            this.PLAYER2IP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConsoleMessage
+            // 
+            this.ConsoleMessage.BackColor = System.Drawing.Color.Gainsboro;
+            this.ConsoleMessage.Location = new System.Drawing.Point(12, 12);
+            this.ConsoleMessage.Multiline = true;
+            this.ConsoleMessage.Name = "ConsoleMessage";
+            this.ConsoleMessage.ReadOnly = true;
+            this.ConsoleMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ConsoleMessage.Size = new System.Drawing.Size(495, 426);
+            this.ConsoleMessage.TabIndex = 0;
+            // 
+            // PLAYER1IP
+            // 
+            this.PLAYER1IP.Font = new System.Drawing.Font("Microsoft JhengHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PLAYER1IP.Location = new System.Drawing.Point(522, 76);
+            this.PLAYER1IP.Name = "PLAYER1IP";
+            this.PLAYER1IP.Size = new System.Drawing.Size(275, 37);
+            this.PLAYER1IP.TabIndex = 1;
+            this.PLAYER1IP.Text = "PLAYER1 : 255.255.255.255";
+            this.PLAYER1IP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // GameStatusLabel
+            // 
+            this.GameStatusLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.GameStatusLabel.Location = new System.Drawing.Point(522, 27);
+            this.GameStatusLabel.Name = "GameStatusLabel";
+            this.GameStatusLabel.Size = new System.Drawing.Size(275, 37);
+            this.GameStatusLabel.TabIndex = 2;
+            this.GameStatusLabel.Text = "Status : Waiting For Player";
+            this.GameStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // PLAYER2IP
+            // 
+            this.PLAYER2IP.Font = new System.Drawing.Font("Microsoft JhengHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PLAYER2IP.Location = new System.Drawing.Point(522, 113);
+            this.PLAYER2IP.Name = "PLAYER2IP";
+            this.PLAYER2IP.Size = new System.Drawing.Size(275, 36);
+            this.PLAYER2IP.TabIndex = 3;
+            this.PLAYER2IP.Text = "PLAYER2 : 255.255.255.255";
+            this.PLAYER2IP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // serverGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PLAYER2IP);
+            this.Controls.Add(this.GameStatusLabel);
+            this.Controls.Add(this.PLAYER1IP);
+            this.Controls.Add(this.ConsoleMessage);
+            this.Name = "serverGUI";
+            this.Text = "server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox ConsoleMessage;
+        private System.Windows.Forms.Label PLAYER1IP;
+        private System.Windows.Forms.Label GameStatusLabel;
+        private System.Windows.Forms.Label PLAYER2IP;
+    }
+}
+
--- multiplayer_Server/multiplayer_Server/Form1.cs
+++ multiplayer_Server/multiplayer_Server/Form1.cs
@@ -23,6 +23,8 @@ namespace multiplayer_Server {
         private delegate void DelUpdateMessage(string sMessage);
         private delegate void DelUpdateSocket(Socket s, int c);
         private delegate void DelPacketAnalyze(Packet pkt);
+        private delegate void DelGUIUpdate(int c, string s);
+        
 
         Socket player1 = null;
         Socket player2 = null;
@@ -31,11 +33,13 @@ namespace multiplayer_Server {
         bool gameStart;
         Packet gameStartHold = null;
         DarkChessModel darkChessModel = null;
-        
+
 
 
         public serverGUI() {
             InitializeComponent();
+            GUIUpdate(0);
+
             this.FormClosing += new FormClosingEventHandler(MainForm_Closing);
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -45,12 +49,8 @@ namespace multiplayer_Server {
             Thread listening = new Thread(ListenClientConnect);
             listening.Start();
             updateMessage("Server Initialize Complete");
-
+            GUIUpdate(1);
             gameStart = false;
-
-        }
-
-        private void server_Load(object sender, EventArgs e) {
 
         }
 
@@ -94,17 +94,18 @@ namespace multiplayer_Server {
                         throw new Exception(String.Format("{0}' has disconnected", myClientSocket.RemoteEndPoint.ToString()));
                     }
                     object obj = Packet.Deserialize(result);
-                    if(obj is Packet) {
+                    if (obj is Packet) {
                         packetAnalyze((Packet)obj);
                     } else {
                         updateMessage(String.Format("接收客戶端 {0} 訊息 {1}", myClientSocket.RemoteEndPoint.ToString(), Encoding.ASCII.GetString(result, 0, receiveNumber)));
                     }
                 } catch (Exception ex) {
                     updateMessage(ex.Message);
-                    if(myClientSocket.Connected) {
+                    if (myClientSocket.Connected) {
                         myClientSocket.Shutdown(SocketShutdown.Both);
                         myClientSocket.Close();
                     }
+                    updateMessage(String.Format("Disconnected both two Player"));
                     setPlayer(null, 2);
                     break;
                 }
@@ -121,6 +122,9 @@ namespace multiplayer_Server {
                         player2.Send(pkt.Data);
                     break;
                 case 2:
+                    updateMessage(String.Format("All Player Standby GAMESTART!!"));
+
+                    GUIUpdate(4);
                     Packet pkt2 = new Packet(Command.GAME_START, darkChessModel.getBoard(), darkChessModel.redPiecesTaken, darkChessModel.blackPiecesTaken);
                     pkt2.playerStatusChange = true;
                     player1.Send(Packet.Serialize(pkt2).Data);
@@ -139,20 +143,33 @@ namespace multiplayer_Server {
                 this.Invoke(del, pkt);
                 //同執行緒
             } else {
+                string Player;
+                if (darkChessModel.isPlayer1Turn)
+                    Player = "Player1";
+                else
+                    Player = "Player2";
+
                 switch (pkt.command) {
                     case Command.FLIP:
                         MoveData flipPos = pkt.moveData;
                         darkChessModel.flip(flipPos.fromX, flipPos.fromY);
+
+                        updateMessage(String.Format("{0} FLIP a Chess at {1} , {2} ", Player, flipPos.fromY, flipPos.fromY));
+
                         darkChessModel.isPlayer1Turn = !darkChessModel.isPlayer1Turn;
                         Packet sent = new Packet(Command.UPDATE_BOARD, darkChessModel.getBoard(), darkChessModel.redPiecesTaken, darkChessModel.blackPiecesTaken);
-                        
+
                         if (!darkChessModel.isGameStart) {
                             gameStartHold = sent;
                             bool isPlayer1Black = false;
                             if (darkChessModel.getBoard()[flipPos.fromX, flipPos.fromY] > 0) {
                                 isPlayer1Black = true;
+                                updateMessage(String.Format("ASSIGN Player1 is Black"));
+                                updateMessage(String.Format("ASSIGN Player2 is Red"));
                             } else {
                                 isPlayer1Black = false;
+                                updateMessage(String.Format("ASSIGN Player1 is Red"));
+                                updateMessage(String.Format("ASSIGN Player2 is Black"));
                             }
 
                             darkChessModel.isGameStart = true;
@@ -166,21 +183,39 @@ namespace multiplayer_Server {
                     case Command.MOVE://MOVE
                         bool isMoveValid = darkChessModel.sumbitMove(pkt.moveData);
                         if (isMoveValid) {
+                            updateMessage(String.Format("{0} MOVE a Chess from {1} , {2} to {3} , {4} ", Player, pkt.moveData.fromX, pkt.moveData.fromY, pkt.moveData.toX, pkt.moveData.toY));
                             darkChessModel.isPlayer1Turn = !darkChessModel.isPlayer1Turn;
                             broadcast(new Packet(Command.UPDATE_BOARD, darkChessModel.getBoard(), darkChessModel.redPiecesTaken, darkChessModel.blackPiecesTaken), 1);
                         } else {
+                            updateMessage(String.Format("{0} MOVE FAIL ", Player));
                             if (darkChessModel.isPlayer1Turn) {
                                 player1.Send(Packet.Serialize(new Packet(Command.MOVEFAIL, null)).Data);
                             } else {
                                 player2.Send(Packet.Serialize(new Packet(Command.MOVEFAIL, null)).Data);
                             }
                         }
-                        if(darkChessModel.isBlackWin()){
+                        if (darkChessModel.isBlackWin()) {
+                            updateMessage(String.Format("BLACK is WIN"));
                             //Broadcast isBlackWin
                             broadcast(new Packet(Command.GAME_RESULT, true), 1);
                         } else if (darkChessModel.isRedWin()) {
+                            updateMessage(String.Format("RED is WIN"));
                             //Broadcast !isBlackWin
                             broadcast(new Packet(Command.GAME_RESULT, false), 1);
+                        }
+                        break;
+                    case Command.SURRENDER:
+                        //Black Surrender
+                        if (pkt.playerStatusChange) {
+                            updateMessage(String.Format("BLACK is SURRENDER"));
+                            //Broadcast !isBlackWin
+                            broadcast(new Packet(Command.GAME_RESULT, false), 1);
+
+                            //Red Surrender
+                        } else if (pkt.playerStatusChange) {
+                            updateMessage(String.Format("RED is SURRENDER"));
+                            //Broadcast isBlackWin
+                            broadcast(new Packet(Command.GAME_RESULT, true), 1);
                         }
                         break;
 
@@ -201,20 +236,26 @@ namespace multiplayer_Server {
 
         private void setPlayer(Socket player, int c) {
             if (this.InvokeRequired) // 若非同執行緒
-{               
+{
                 DelUpdateSocket del = new DelUpdateSocket(setPlayer); //利用委派執行
                 this.Invoke(del, player, c);
                 //同執行緒
             } else {
-                switch(c){
+                string playerip = "";
+                if (player != null)
+                    playerip = IPAddress.Parse(((IPEndPoint)player.RemoteEndPoint).Address.ToString()).ToString();
+                switch (c) {
                     case 1:
-                        if (player1 == null)
+                        if (player1 == null) {
+                            GUIUpdate(2, playerip);
                             player1 = player;
-                        else
+                        } else {
+                            GUIUpdate(3, playerip);
                             player2 = player;
+                        }
                         break;
                     case 2:
-                        if(player1 != null)
+                        if (player1 != null)
                             player1.Close();
                         if (player2 != null)
                             player2.Close();
@@ -224,6 +265,7 @@ namespace multiplayer_Server {
                         connectionNumber = 0;
                         gameStart = false;
                         darkChessModel = null;
+                        GUIUpdate(1);
                         break;
 
                 }
@@ -234,6 +276,37 @@ namespace multiplayer_Server {
             setPlayer(null, 2);
             System.Environment.Exit(0);
 
+        }
+
+        private void GUIUpdate(int c, string s = "") {
+            if (this.InvokeRequired) { // 若非同執行緒
+                DelGUIUpdate del = new DelGUIUpdate(GUIUpdate); //利用委派執行
+                this.Invoke(del, c, s);
+                //同執行緒
+            } else {
+                switch (c) {
+                    case 0://init GUI
+                        GameStatusLabel.Text = "Status : Initializing";
+                        PLAYER1IP.Text = "Player1 :";
+                        PLAYER2IP.Text = "Player2 :";
+                        break;
+                    case 1://reset GUI
+                        GameStatusLabel.Text = "Status : Waiting For Player";
+                        PLAYER1IP.Text = "Player1 :";
+                        PLAYER2IP.Text = "Player2 :";
+                        break;
+                    case 2://set Player1 IP
+                        PLAYER1IP.Text = String.Format("PLAYER1 : {0}", s);
+                        break;
+                    case 3://set Player2 IP
+                        PLAYER2IP.Text = String.Format("PLAYER2 : {0}", s);
+                        break;
+                    case 4://GameStart
+                        GameStatusLabel.Text = "Status : Gaming";
+                        break;
+
+                }
+            }
         }
     }
 }
